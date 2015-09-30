5ffccf747121a292b9f13f927b3bf76f4e887d44
ehawk823 <evan.alexander.hawk@gmail.com>
2015-09-30 11:07:28 -0500

    added location form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   def update
     current_user.name = params[:user][:name]
     @name = current_user.name
+    current_user.location = params[:user][:location]
+    @location = current_user.location
   end
 
 end
