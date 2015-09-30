05db435bc9b989be7e8e8f981f39cbc7b8c8c118
ehawk823 <evan.alexander.hawk@gmail.com>
2015-09-30 12:01:21 -0500

    created location and mission forms on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,26 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.name = params[:user][:name]
-    @name = current_user.name
-    current_user.location = params[:user][:location]
-    @location = current_user.location
+    if params[:user][:name] == nil
+      @name = current_user.name
+    else
+      current_user.name = params[:user][:name]
+      @name = current_user.name
+      current_user.save
+    end
+    if params[:user][:location] == nil
+      @location = current_user.location
+    else
+      current_user.location = params[:user][:location]
+      @location = current_user.location
+      current_user.save
+    end
+    if params[:user][:bio] == nil
+      @mission = current_user.bio
+    else
+      current_user.bio = params[:user][:bio]
+      @mission = current_user.bio
+      current_user.save
+    end
   end
-
 end
