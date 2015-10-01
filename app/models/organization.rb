a58f078b0a029746f898acdd626344b768677cd3
George Toothman <george.toothman@flatironschool.com>
2015-10-01 15:44:21 -0400

    add organization model and tests

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,12 +1,19 @@
 class Organization
   include OrganizationsHelper
   include Change::Resources
-  attr_accessor :client, :organization, :signature_count, :signature_count
+  attr_accessor :client, :organization, :org_id
 
   def initialize
     @client = Change::Requests::Client.new({ :api_key => ENV["CHANGE_KEY"], :secret_token => ENV["CHANGE_SECRET"] })
     @organization = Organization.new(@client)
-    @loaded_organization = @organization.load("https://www.change.org/p/ellen-degeneres-dear-ellen-cut-out-leather-from-your-new-shoe-line")
-    @org_id = @loaded_petition['organization_id']
+    binding.pry
+    @loaded_organization = @organization.load(pick_organization)
+    @name = @loaded_petition['name']
+    @website = @loaded_petition['website'] 
+    @change_org_url = @loaded_petition['organization_url']
+  end
+
+  def pick_organization
+    @organization_url = return_organizations_array.sample
   end
 end
